e4e075308323f4b73fe54f9fefa637a920fa9e51
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-08-03 17:43:50 -0400

    reformatted object

--- actions/lib/utils.py
+++ actions/lib/utils.py
@@ -17,11 +17,11 @@
 class Device:
     def __init__(self, light):
         try:
+            self.label = light.get_label()
             self.mac_addr = light.get_mac_addr()
             self.ip_addr = light.get_ip_addr()
             self.color = str(light.get_color())
-            self.power_level = light.power_level
-            self.label = light.get_label()
+            self.power_level = light.power_level          
             self.product_name = light.get_product_name()
             self.product_features = light.get_product_features()
         except AttributeError as e:
@@ -29,11 +29,11 @@ class Device:
 
     def to_dict(self):
         return {
+            'label': self.label,
             'mac_addr': self.mac_addr,
             'ip_addr': self.ip_addr,
             'color': self.color,
             'power_level': self.power_level,
-            'label': self.label,
             'product_name': self.product_name,
             'product_features': self.product_features
         }
