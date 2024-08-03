9d4814c01155cc37d6620a6f291062a496ed3f58
Fabricio E. Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2024-08-03 13:17:08 -0400

    optimized and refactored

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -17,21 +17,15 @@
 from st2common.runners.base_action import Action
 from lifxlan import Light
 
-class Colors:
-    """Defines preset colors for LIFX lights."""
-    def __init__(self):
-        self.theatre = (0, 65535, 43973, 2500)
-        self.evening = (5461, 48495, 65535, 3500) #(35, 65535, 32767, 2500)
-        self.daylight = (0, 0, 65535, 5600)
-
 class BaseAction(Action):
     """Defines a base action class for managing LIFX lights based on preset colors and brightness levels."""
     def __init__(self, config):
         super(BaseAction, self).__init__(config)
         self.lights_config = self.config.get('lights', [])
         self.rooms = self.config.get('rooms', [])
+        self.scenes = self.config.get('scenes', {})
         self.lights = {}
-
+        
         for light in self.lights_config:
             self.lights[light['name']] = Light(light['mac'], light['ip'])
         self.logger.info('LIFX lights initialized')
--- actions/set_scene.py
+++ actions/set_scene.py
@@ -21,29 +21,33 @@ from lifxlan import WorkflowException
 class SetScene(action.BaseAction):
     def run(self, **paramaters):
         rooms = paramaters.get("rooms", [])
-        self.color = paramaters.get("color", "daylight")
-        self.brightness = paramaters.get("brightness", "full")
-
-        if self.brightness < 0 or self.brightness > 100:
+        scene = paramaters.get("scene", None)
+        self.scene = self.scenes.get(scene, None)
+        self.logger.debug(f"Scene: {self.scene}")
+      
+        # * convert the brightness as a percentage to a range of 16 bit values (0-65535)
+        if self.scene["brightness"] < 0 or self.scene["brightness"] > 100:
             raise ValueError("Brightness must be between 0 and 100%")
-        self.brightness = int((self.brightness / 100) * 65535)
+        self.brightness = int((self.scene["brightness"] / 100) * 65535)
     
         for room_name in rooms:
             self.set_room_lights(room_name)
         return True
 
+    # Sets the lights in a given room to the desired scene settings.
+    # If a timeout to the response occurs, we try to set the light scene one more time. 
     def set_room_lights(self, room_name):
         for room in self.rooms:
             if room["name"] == room_name:
                 lights = room["lights"]
                 for light in lights:
                     try:
-                        self.lights[light].set_color(self.color, 500)
+                        self.lights[light].set_color(self.scene["color"], 500)
                         self.lights[light].set_brightness(self.brightness, 500)
                     except WorkflowException as err:
                         self.logger.warning(f"Timeout setting light {light} in {room_name}: {err}")
                         try:
-                            self.lights[light].set_color(self.color, 500)
+                            self.lights[light].set_color(self.scene["color"], 500)
                             self.lights[light].set_brightness(self.brightness, 500)
                         except Exception as err:
                             self.logger.warning(f"Failed to set light {light} in {room_name}: {err}")
@@ -51,7 +55,7 @@ class SetScene(action.BaseAction):
                     except Exception as err:
                         self.logger.error(f"Failed to set light {light} in {room_name}: {err}")
                         sys.exit(1)
-                self.logger.info(f"Successfully set lights in the {room_name} to the daylight scene.")
+                self.logger.info(f"Successfully set lights in the {room_name} to the requested scene.")
                 break
         else:
             self.logger.warning(f"Room {room_name} not found.")
